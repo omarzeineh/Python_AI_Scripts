5654741ebc549f002690fa29d419f241a91b9c91
Omar Zeineh <165347268+omarzeineh@users.noreply.github.com>
2025-08-27 11:44:29 +0400

    Update polynomial_regression_plot.py

--- polynomial_regression_plot.py
+++ polynomial_regression_plot.py
@@ -5,7 +5,7 @@ from sklearn.metrics import root_mean_squared_error
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.linear_model import LinearRegression
 
-df = pd.read_csv('D:\\uni stuff\\2024-2025\\Spring 2024-2025\\AIRE310\\data.csv')
+df = pd.read_csv('data.csv')
 
 x = df.iloc[:, 0].values
 y = df.iloc[:, 1].values
@@ -55,4 +55,5 @@ y = model.predict(poly.fit_transform(x.reshape(-1, 1)))
 plt.plot(x, y)
 plt.xlabel('x')
 plt.ylabel('y')
+
 plt.show()
